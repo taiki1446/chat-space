5ea5cabfb325ded348fee8b1f3343b3352e7c525
taiki1446 <kbkr0921@gmail.com>
2019-11-14 12:22:29 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,7 +10,7 @@ $(function(){
                         ${message.created_at}
                       </div>
                     </div>
-                    <div class="main__messages">
+                    <div class="main__messages" data-messageid=${message.message_id}>
                       <p class="__message">
                         ${message.content}
                       </p>`;
@@ -25,6 +25,40 @@ $(function(){
 
   function disabled(){
     $(".main__footer__form__send").prop("disabled", false);
+  };
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let messagesObj = $(".main__messages");
+    let last_message_id = 0;
+    let comparison;
+    messagesObj.filter(function(){
+      comparison = $(this).data("messageid");
+      if (comparison > last_message_id){
+        last_message_id = comparison;
+      };
+    });    
+
+    let url = location.href.match(/\/groups\/[\d]{1,}\/messages/);
+    url = url[0].replace("/messages", "/api/messages");
+
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //配列messagesの中身一つ一つを取り出し、HTMLに追加する。
+      messages.forEach(function(message){
+        $(".main").append(messageHtml(message));
+      });
+      $(".main").animate({scrollTop: $(".main")[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
   };
 
   $(".messageForm").on("submit", function(e){
@@ -51,6 +85,11 @@ $(function(){
       alert("メッセージ送信に失敗しました");
       disabled();
     });
-
   });
+
+  let url_updata = location.href.match(/\/groups\/[\d]{1,}\/messages/);
+  console.log(url_updata);
+  if (url_updata !== null){
+    setInterval(reloadMessages, 7000);
+  };
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.user_name @message.user.name
 json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content @message.content
 json.image_url @message.image.url
+json.message_id @message.id
